8df28610afa8f3136e97579f8eabcfe218294770
nawabsahab16 <117763400+nawabsahab16@users.noreply.github.com>
2023-06-06 22:36:02 +0530

    updated the ui

--- lilgame/src/App.js
+++ lilgame/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 
+import {Box} from "./components/Box"
 
 function App() {
   return (
     <div className="App">
-      Hey There!!
+      <Box/>
     </div>
   );
 }
---
+++ lilgame/src/components/Box.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import "./Box.css"
+
+
+export const Box = () => {
+  return (
+    <button className="box">X</button>
+  )
+}
